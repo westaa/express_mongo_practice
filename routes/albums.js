e35c70cc8717b3be9615c21cca962b8f656c7e6e
Andrew West <westaa@colorado.edu>
2016-12-09 17:57:55 -0700

    Mongo Full Crud album-store

--- routes/albums.js
+++ routes/albums.js
@@ -3,28 +3,25 @@ var router = express.Router();
 var db = require('monk')('localhost/album-demo');
 var albumCollection = db.get('albums');
 
-router.get('/albums', function(req, res, next) {
-  res.render('albums/index');
-});
-
 router.get('/albums/new', function(req, res, next) {
   res.render('albums/new');
 });
 
 router.post('/albums', function(req, res, next) {
-  albumCollection.insert({ name: req.body.album_name });
+  console.log(req.body);
+  albumCollection.insert({ name: req.body.album_name, artist: req.body.album_artist, year: req.body.album_year, genre: req.body.album_genre});
   res.redirect('/albums');
 });
-
-router.get('/albums', function(req, res, next) {
-  albumCollection.find({}, function (err, records) {
-    res.render('albums/index', {allAlbums: records});
-  });
-});
+//
+// router.get('/albums', function(req, res, next) {
+//   albumCollection.find({}, function (err, records) {
+//     res.render('albums/index', {allAlbums: records});
+//   });
+// });
 
 router.get('/albums/:id', function(req, res, next) {
   albumCollection.findOne({_id: req.params.id}, function (err, record) {
-    res.render('albums/show', {theAlbum: record});
+    res.render('albums/edit', {theAlbum: record});
     console.log(theAlbum);
   });
 });
@@ -35,16 +32,25 @@ router.get('/albums/:id/edit', function(req, res, next) {
   });
 });
 
-router.post('/albums/:id/undefined/edit', function(req, res, next) {
-  albumCollection.update({_id: req.params.id}, {$set: {name: req.body.name}});
+router.get('/albums/:id/delete', function(req, res, next) {
+  albumCollection.remove({_id: req.params.id});
+  albumCollection.find({}, function (err, records) {
+    res.render('albums/index', {allAlbums: records});
+  });
+});
+
+router.post('/albums/:id/edit', function(req, res, next) {
+  albumCollection.update({_id: req.params.id}, {$set: { name: req.body.album_name, artist: req.body.album_artist, year: req.body.album_year, genre: req.body.album_genre}});
   albumCollection.find({}, function (err, records) {
     res.render('albums/index', {allAlbums: records});
   });
 });
 
 router.post('/albums', function(req, res, next) {
-  albumCollection.insert({ name: req.body.album_name });
+  albumCollection.insert({ name: req.body.album_name, artist: req.body.artist, year: req.body.year, genre: req.body.genre});
   res.redirect('/albums');
 });
+
+
 
 module.exports = router;
